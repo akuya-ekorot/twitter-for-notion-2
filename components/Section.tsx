782de38917efe63f015d45fd6a2176de25a05d11
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-06 22:19:02 +0300

    feat: link sections together

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,8 +1,21 @@
-const Section = ({ title, buttonText }) => {
+interface SectionProps {
+  title: string;
+  buttonText: string;
+  id: string;
+  linkToSection?: string;
+}
+
+const Section = ({ title, buttonText, id, linkToSection }: SectionProps) => {
   return (
-    <div className="flex flex-col text-2xl gap-6 w-screen h-screen items-center justify-center">
+    <div
+      id={id}
+      className="flex flex-col items-center justify-center w-screen h-screen text-2xl gap-6"
+    >
       <h1>{title}</h1>
-      <a className="px-5 py-4 rounded text-base bg-black text-white">
+      <a
+        href={linkToSection && `#${linkToSection}`}
+        className="px-5 py-4 text-base text-white bg-black rounded"
+      >
         {buttonText}
       </a>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,25 @@ import Section from "../components/Section";
 
 const Home = () => {
   return (
-    <Section
-      title={`Welcome to Twitter for Notion`}
-      buttonText={`Get Started`}
-    />
+    <>
+      <Section
+        title={`Welcome to Twitter for Notion`}
+        buttonText={`Get Started`}
+        id={`get-started`}
+        linkToSection={`twitter`}
+      />
+      <Section
+        title={`Connect your Twitter account`}
+        buttonText={`Log in to Twitter`}
+        id={`twitter`}
+        linkToSection={`notion`}
+      />
+      <Section
+        title={`Let's set up your Notion`}
+        buttonText={`Connect to Notion`}
+        id={`notion`}
+      />
+    </>
   );
 };
 
