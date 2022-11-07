c0dbbdb1b7492ffb8c22fba0b1790dfa1ee20aec
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-07 22:35:06 +0300

    feat: finish notion authentication process

--- pages/api/notion/auth.ts
+++ pages/api/notion/auth.ts
@@ -4,9 +4,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  res
-    .status(200)
-    .redirect(
-      "https://api.notion.com/v1/oauth/authorize?client_id=40b91009-9771-41f8-8104-ed2f9856cac3&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fnotion%2Fauth"
-    );
+  res.status(200).redirect(String(process.env.NOTION_AUTH_URL));
 }
