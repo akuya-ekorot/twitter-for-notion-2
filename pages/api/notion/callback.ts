8ec185f29a04b4c270c332a690db9e2cefe43705
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-07 18:31:21 +0300

    bug: add req and res types

--- pages/api/notion/callback.ts
+++ pages/api/notion/callback.ts
@@ -1,3 +1,8 @@
-export default async function handler(req, res) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   res.status(200).send("You called back!");
 }
