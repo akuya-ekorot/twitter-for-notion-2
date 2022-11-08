e4858ca33551eb3f910597e7c2c605b07d40118f
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-08 12:02:40 +0300

    chore: delete notion auth.ts file

--- pages/api/notion/auth.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).redirect(String(process.env.NOTION_AUTH_URL));
-}
